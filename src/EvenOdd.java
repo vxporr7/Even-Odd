76cd5a31da7439062006f70a2929d9ee73008709
Kunmi Aaron <aaronob@mail.uc.edu>
2024-09-20 14:17:43 -0400

    even odd

---
+++ src/EvenOdd.java
@@ -0,0 +1,8 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        System.out.println("Want to see if your number is even or odd?? Input it below!");
+        int numToExamine = 2;
+// program will determine whether the inout is even or odd.
+        System.out.println(" the number you entered is even");
+    }
+}
