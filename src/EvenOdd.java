10c77a457e25a466199d487af5cd83caea0993c6
Kunmi Aaron <aaronob@mail.uc.edu>
2024-09-20 14:20:29 -0400

    even odd

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -2,7 +2,7 @@ public class EvenOdd {
     public static void main(String[] args) {
         System.out.println("Want to see if your number is even or odd?? Input it below!");
         int numToExamine = 2;
-// program will determine whether the inout is even or odd.
+// program will then determine that since 2 mod 2 is ) that numToExamine is even.
         System.out.println(" the number you entered is even");
     }
 }
